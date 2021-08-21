7eddf90c5eb1a6d78889843e2d27e4cf0e8c036c
Hsin Yu <winter0462@gmail.com>
2021-08-21 22:59:52 +0800

    optimize the last chance

--- password.py
+++ password.py
@@ -5,10 +5,14 @@ count = 3
 
 # 讓使用者輸入密碼，最多三次
 while count > 0:
+	count = count - 1
 	p = input('請輸入密碼: ')
 	if p == password:
 		print('登入成功!')
 		break
 	else:
-		count = count - 1
-		print('密碼錯誤，還有', count, '次機會')
+		print('密碼錯誤!')
+		if count > 0:
+			print('還有', count, '次機會')
+		else:
+			print('請重新設定密碼')
